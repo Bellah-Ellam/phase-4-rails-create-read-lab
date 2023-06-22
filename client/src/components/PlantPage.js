8f9b9ce00357c60c78e4f85301145befe35e4cb8
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-06-22 22:53:40 +0300

    complete assignment

--- client/src/components/PlantPage.js
+++ client/src/components/PlantPage.js
@@ -9,9 +9,10 @@ function PlantPage() {
 
   useEffect(() => {
     // no need to use http://localhost:3000 here
-    fetch("/plants")
+    fetch("http://localhost:3000/plants")
       .then((r) => r.json())
       .then((plantsArray) => {
+        console.log(plantsArray); // Check the value of plantsArray
         setPlants(plantsArray);
       });
   }, []);
